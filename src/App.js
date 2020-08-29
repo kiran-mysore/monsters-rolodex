713d811146ba095afe33b1b533f3cf5b227bc1cb
Kiran Rao <kiran.mysore.rao@gmail.com>
2020-08-29 17:22:10 +0200

    Adding initial pages to github pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { CardList } from '../src/components/card-list/card-list.component';
+import { SearchBox } from '../src/components/search-box/search-box.component';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    console.log('Inside constructor');
+    this.state = {
+      monsters: [],
+      searchField: '',
+    };
+  }
+  componentDidMount() {
+    console.log('Inside Mount');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1> Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +59,6 @@ function App() {
       </header>
     </div>
   );
-}
+} */
 
 export default App;
