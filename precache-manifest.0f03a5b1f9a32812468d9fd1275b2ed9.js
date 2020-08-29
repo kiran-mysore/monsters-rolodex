21fbef13da579c2cf04512180816d9df14d5bda1
Kiran Rao <kiran.mysore.rao@gmail.com>
2020-08-29 17:21:00 +0200

    Updates

---
+++ precache-manifest.0f03a5b1f9a32812468d9fd1275b2ed9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "373b876e1997ead28490cd30a54ec44c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8ebf123a8cadcdf14418",
+    "url": "/monsters-rolodex/static/css/main.fa23a8b9.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ebf123a8cadcdf14418",
+    "url": "/monsters-rolodex/static/js/main.bfa26836.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
